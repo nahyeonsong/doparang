2e7b6851f85c329f3fd59af32763d55f6284735f
Nahyeon Song <sny0708@naver.com>
2023-06-17 03:40:16 +0900

    세번째 커밋

--- pybo/views.py
+++ pybo/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
+from django.http import HttpResponseNotAllowed
+from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 
 def index(request):
+    page = request.GET.get("page", "1")  # 페이지
     question_list = Question.objects.order_by("-create_date")
-    context = {"question_list": question_list}
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {"question_list": page_obj}
     return render(request, "pybo/question_list.html", context)
 
 
@@ -16,11 +22,36 @@ def detail(request, question_id):
 
 
 def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(
-        content=request.POST.get("content"), create_date=timezone.now()
-    )
-    return redirect("pybo:detail", question_id=question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect("pybo:detail", question_id=question.id)
+    else:
+        return HttpResponseNotAllowed("Only POST is possible.")
+    context = {"question": question, "form": form}
+    return render(request, "pybo/question_detail.html", context)
+
+
+def question_create(request):
+    if request.method == "POST":
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect("pybo:index")
+    else:
+        form = QuestionForm()
+    context = {"form": form}
+    return render(request, "pybo/question_form.html", context)
 
 
 # Create your views here.
