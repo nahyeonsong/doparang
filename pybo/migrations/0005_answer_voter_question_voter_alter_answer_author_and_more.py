8abdf4af8357a5823b6ab936b13a20da85d33aac
Nahyeon Song <sny0708@naver.com>
2023-06-23 20:24:19 +0900

    반대1

--- pybo/migrations/0005_answer_voter_question_voter_alter_answer_author_and_more.py
+++ pybo/migrations/0005_answer_voter_question_voter_alter_answer_author_and_more.py
@@ -6,31 +6,74 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('pybo', '0004_answer_modify_date_question_modify_date'),
+        ("pybo", "0004_answer_modify_date_question_modify_date"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='answer',
-            name='voter',
-            field=models.ManyToManyField(related_name='voter_answer', to=settings.AUTH_USER_MODEL),
+            model_name="answer",
+            name="voter",
+            field=models.ManyToManyField(
+                related_name="voter_answer", to=settings.AUTH_USER_MODEL
+            ),
         ),
         migrations.AddField(
-            model_name='question',
-            name='voter',
-            field=models.ManyToManyField(related_name='voter_question', to=settings.AUTH_USER_MODEL),
+            model_name="question",
+            name="voter",
+            field=models.ManyToManyField(
+                related_name="voter_question", to=settings.AUTH_USER_MODEL
+            ),
+        ),
+        migrations.AddField(
+            model_name="answeropp",
+            name="voteropp",
+            field=models.ManyToManyField(
+                related_name="voteropp_answeropp", to=settings.AUTH_USER_MODEL
+            ),
+        ),
+        migrations.AddField(
+            model_name="questionopp",
+            name="voteropp",
+            field=models.ManyToManyField(
+                related_name="voteropp_questionopp", to=settings.AUTH_USER_MODEL
+            ),
         ),
         migrations.AlterField(
-            model_name='answer',
-            name='author',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='author_answer', to=settings.AUTH_USER_MODEL),
+            model_name="answer",
+            name="author",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="author_answer",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
         migrations.AlterField(
-            model_name='question',
-            name='author',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='author_question', to=settings.AUTH_USER_MODEL),
+            model_name="question",
+            name="author",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="author_question",
+                to=settings.AUTH_USER_MODEL,
+            ),
+        ),
+        migrations.AlterField(
+            model_name="answeropp",
+            name="authoropp",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="authoropp_answeropp",
+                to=settings.AUTH_USER_MODEL,
+            ),
+        ),
+        migrations.AlterField(
+            model_name="questionopp",
+            name="authoropp",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="authoropp_questionopp",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
     ]
