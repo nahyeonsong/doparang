ada1f208db370e945d4a270d683cb3dd6994e357
Nahyeon Song <sny0708@naver.com>
2023-06-24 15:51:44 +0900

    이미지2

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -2,10 +2,10 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .forms import PostForm
 
 from ..forms import QuestionForm
 from ..models import Question
+from ..forms import PostForm
 
 
 @login_required(login_url="common:login")
