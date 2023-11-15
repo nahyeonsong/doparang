38297eb86ba1f357c32f140c48ec887859ce373f
Nahyeon Song <sny0708@naver.com>
2023-11-15 22:33:58 +0900

    해결각 2

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,7 +2,7 @@ import environ
 
 from .base import *
 
-ALLOWED_HOSTS = ["43.202.10.101", "doparang.com"]
+ALLOWED_HOSTS = ["43.202.10.101"]
 STATIC_ROOT = BASE_DIR / "static/"
 STATICFILES_DIRS = []
 DEBUG = True
